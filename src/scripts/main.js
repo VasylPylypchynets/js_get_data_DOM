3ccc0ed0dc0a67cd6f562cace1aa427f257acf7c
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-14 16:07:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+
+const populationElements = document.querySelectorAll('.population');
+
+const populations = Array.from(populationElements).map((pop) => {
+  return Number(pop.textContent.split(',').join(''));
+});
+
+const totalPopulation = populations.reduce((sum, population) => {
+  return sum + population;
+}, 0);
+
+const averagePopulation = totalPopulation / populations.length;
+
+const formattedTotal = totalPopulation.toLocaleString();
+const formattedAverage = Math.round(averagePopulation).toLocaleString();
+
+document.querySelector('.total-population').textContent = formattedTotal;
+document.querySelector('.average-population').textContent = formattedAverage;
